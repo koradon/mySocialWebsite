11df62f0d292a2667a2c8d4036d5187070bc0968
EMICSIP <michal.sipa@ericsson.com>
2017-03-30 15:38:13 +0200

    Added list.html, list_ajax.html and endles scrolling using ajax.

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 
 from common.decorators import ajax_required
@@ -12,6 +13,39 @@ from .models import Image
 
 
 # Create your views here.
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 20)
+    page = request.GET.get('page')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if request.is_ajax():
+        context = {
+            'section': 'images',
+            'images': images,
+        }
+        return render(request,
+                      'images/image/list_ajax.html',
+                      context)
+
+    context = {
+        'section': 'images',
+        'images': images,
+    }
+    return render(request,
+                  'images/image/list.html',
+                  context)
+
+
 @login_required
 def image_create(request):
     if request.method == 'POST':
